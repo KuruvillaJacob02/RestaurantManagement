828dcfdea15dca3441ccb8f1e10f52c7dbd82830
Kuruvilla Jacob <36655830+KuruvillaJacob02@users.noreply.github.com>
2023-12-17 19:20:43 +0530

    Update admin.py

--- dbms/restaurant/admin.py
+++ dbms/restaurant/admin.py
@@ -1,10 +1,10 @@
 # restaurant/admin.py
 
 from django.contrib import admin
-from .models import Table, Menu, OrderItems, Order, Transaction
+from .models import Table, Menu, Order, Transaction
 
 admin.site.register(Table)
 admin.site.register(Menu)
-admin.site.register(OrderItems)
+#admin.site.register(OrderItems)
 admin.site.register(Order)
 admin.site.register(Transaction)
